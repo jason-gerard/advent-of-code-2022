1767d1db885f9580a34ecea2089d063fbe159695
Jason Gerard <jasongerard321@gmail.com>
2022-12-02 09:51:48 -0500

    Finish day1 part 2 question

--- src/bin/day1part1.rs
+++ src/bin/day1part1.rs
@@ -2,7 +2,6 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    println!("Hello, world! Day1");
     let path = Path::new("./src/bin/day1-input.txt");
     let contents = fs::read_to_string(path)
         .expect("Could not read the file");
---
+++ src/bin/day1part2.rs
@@ -0,0 +1,25 @@
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("./src/bin/day1-input.txt");
+    let contents = fs::read_to_string(path)
+        .expect("Could not read the file");
+
+    let elf_sums: Vec<i32> = contents
+        .trim()
+        .split("\n\n")
+        .map(|calorie_lines| calorie_lines
+            .split("\n")
+            .map(|calorie_line| calorie_line.parse::<i32>().unwrap())
+            .sum())
+        .collect();
+
+    let mut elf_sums_sorted = elf_sums.clone();
+    elf_sums_sorted.sort();
+
+    println!("{:?}", elf_sums_sorted);
+    let most_calories: i32 = elf_sums_sorted.iter().rev().take(3).sum();
+
+    println!("{}", most_calories);
+}
