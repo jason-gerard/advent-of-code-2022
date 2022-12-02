ad323c6f2b6fb8444fd0863367d29efa84cf8227
Jason Gerard <jasongerard321@gmail.com>
2022-12-02 09:34:03 -0500

    Finish day1 part 1 problem

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -7,5 +7,17 @@ fn main() {
     let contents = fs::read_to_string(path)
         .expect("Could not read the file");
 
-    println!("{}", contents);
+    let elf_sums: Vec<i32> = contents
+        .trim()
+        .split("\n\n")
+        .map(|calorie_lines| calorie_lines
+            .split("\n")
+            .map(|calorie_line| calorie_line.parse::<i32>().unwrap())
+            .sum())
+        .collect();
+
+    let most_calories = elf_sums.iter().max().unwrap();
+    let index = elf_sums.iter().position(|element| element == most_calories).unwrap();
+
+    println!("{} at index {}", most_calories, index);
 }
