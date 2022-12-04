0667156fedc60a7a187fcc487e0732cc95a6ebd5
Jason Gerard <jasongerard321@gmail.com>
2022-12-04 01:23:43 -0500

    Improve day 4 part 2 code

--- src/bin/day4part2.rs
+++ src/bin/day4part2.rs
@@ -6,26 +6,22 @@ fn main() {
     let contents = fs::read_to_string(path)
         .expect("Could not read the file");
 
-    let count: u32 = contents
+    let count = contents
         .trim()
         .split("\n")
         .map(|line| {
             // [[start, end],[start, end]]
-            let mut x = line.split(",").map(|part| part.split("-"));
-
-            let mut first = x.next().unwrap();
-            let first_start = first.next().unwrap().parse::<u32>().unwrap();
-            let first_end = first.next().unwrap().parse::<u32>().unwrap();
-
-            let mut second = x.next().unwrap();
-            let second_start = second.next().unwrap().parse::<u32>().unwrap();
-            let second_end = second.next().unwrap().parse::<u32>().unwrap();
+            let ranges = line
+                .split(",")
+                .flat_map(|part| part.split("-"))
+                .map(|val| val.parse::<u32>().unwrap())
+                .collect::<Vec<_>>();
 
             // if first end < second start or second end < first start
             // they dont overlap
-            if !(first_end < second_start || second_end < first_start) { 1 } else { 0 }
+            if !(ranges[1] < ranges[2] || ranges[3] < ranges[0]) { 1 } else { 0 }
         })
-        .sum();
+        .sum::<u32>();
 
-    println!("{} number of ranges", count);
+    println!("{} number of ranges overlap", count);
 }
